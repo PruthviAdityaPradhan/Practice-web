994689c7173c6e2fb2b233e03bfbe0cd7898670c
pruthvi <pradhanpruthviaditya@gmail.com>
2024-03-19 19:19:57 +0530

    JS

--- JavaScript/Practice-1/app.js
+++ JavaScript/Practice-1/app.js
@@ -11,3 +11,10 @@ let fruits = ["apple","banana","mango" ];
 console.log(fruits);
 fruits[0] = "guava";
 console.log(fruits);
+
+
+//splice
+let months = ["jan","feb","march"];
+console.log(months);
+months.splice(0,2,"oct");
+console.log(months);
