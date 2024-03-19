a8040e61472d21a5e858bc47c177d5b7a0451135
pruthvi <pradhanpruthviaditya@gmail.com>
2024-03-19 17:15:21 +0530

    JS

--- JavaScript/Practice-1/app.js
+++ JavaScript/Practice-1/app.js
@@ -5,3 +5,9 @@ let names = ["bob","joe","john"];
 let info = ["abc",1,2.5]; //mixed array
 
 let newArr = [];  //empty array
+
+//Arrays are mutable
+let fruits = ["apple","banana","mango" ];
+console.log(fruits);
+fruits[0] = "guava";
+console.log(fruits);
