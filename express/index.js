f43fb151ebf851cd083f00be5a674a5b13b100d7
pruthvi <pradhanpruthviaditya@gmail.com>
2024-06-27 00:41:49 +0530

    creating routes

--- express/index.js
+++ express/index.js
@@ -3,8 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
+
+app.get('/help', (req, res) => {
+    res.send('You contacted help');
+  })
+
+app.get('/home', (req, res) => {
+    res.send('You contacted home');
+  })  
+
+app.get('/contacts', (req, res) => {
+    res.send('You searched contacts');
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
