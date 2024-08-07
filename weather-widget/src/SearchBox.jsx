57297a7bf7717841dcb4719885ccd263fed1befd
pruthvi <pradhanpruthviaditya@gmail.com>
2024-08-07 13:08:56 +0530

    using weather api

--- weather-widget/src/SearchBox.jsx
+++ weather-widget/src/SearchBox.jsx
@@ -5,6 +5,15 @@ import { useState } from 'react';
 
 export default function SearchBox(){
     let [city, setCity] = useState("");
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "dc48102e7b424805a2f63b3c26e86113";
+
+    let getWeatherInfo = async () =>{
+        let response = await(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+    };
+
+    
 
     let handleChange = (evt) => {
         setCity(evt.target.value);
@@ -12,7 +21,8 @@ export default function SearchBox(){
 
     let handleSubmit = (evt) =>{
         evt.preventDefault();
-        setCity("")
+        setCity("");
+        getWeatherInfo();
     } 
 
     return(
